5c206a16beaef132c4a72f4f7ded746fbe4ede2f
Nabil Mansour <nabilmansour1999@gmail.com>
2024-07-22 23:17:59 -0400

    app will now redirect to landing for new users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { currentUser } from "@clerk/nextjs/server";
 import LandingPageComponent from "./ui/components/pages/LandingPageComponent";
 import HomePageComponent from "./ui/components/pages/HomePageComponent";
+import { redirect } from "next/navigation";
 
 export default async function HomePage({ searchParams }: { searchParams: SearchParams }) {
   const user = await currentUser();
   searchParams["page"] = searchParams["page"] ?? "1";
 
-  return user ?
-    <HomePageComponent
-      searchParams={searchParams}
-      name={user.fullName}
-      clerkId={user.id} /> :
-    <LandingPageComponent />
+  if (!user) {
+    redirect("/landing");
+  }
+
+  return <HomePageComponent searchParams={searchParams} name={user.fullName} clerkId={user.id} />
 }
