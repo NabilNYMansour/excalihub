72549a8ba41ee3c9e3b454e6fc6fcbbfcb867604
Nabil NY Mansour <56453977+NabilNYMansour@users.noreply.github.com>
2025-02-16 01:52:22 -0500

    readded the public dev key to fix error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "no-clerk",
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_ZGVsaWNhdGUtd3Jlbi02Ni5jbGVyay5hY2NvdW50cy5kZXYk",
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY || "no-clerk",
     CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET || "no-clerk",
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: '/sign-in',
