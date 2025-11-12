07aa97a6f8b847ecafb4a5fa2baf548a90a2be67
Nabil <nabilmansour1999@gmail.com>
2025-11-11 21:39:31 -0500

    better shaking

--- next.config.js
+++ next.config.js
@@ -6,5 +6,8 @@ module.exports = {
     CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET || "no-clerk",
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: '/sign-in',
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: '/sign-up',
-  }
+  },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/notifications'],
+  },
 }
