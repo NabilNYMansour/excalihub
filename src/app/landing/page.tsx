2c16fdaf8e284f52401ef5704f7cfefb8fb7dced
Nabil <nabilmansour1999@gmail.com>
2025-11-18 20:41:35 -0500

    better landing

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,55 +1,24 @@
-"use client";
-
-import { Box, Divider, Flex, Group } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
-import { Skeleton, useComputedColorScheme } from "@mantine/core";
-import dynamic from "next/dynamic";
-import classes from './page.module.css';
-import { WELCOME_EXCALI_DATA } from '@/lib/constants';
+import { Flex } from '@mantine/core';
 import { FeaturesCards } from '../ui/components/landing/FeaturesCards';
 import LandingHero from '../ui/components/landing/LandingHero';
 import Faq from '../ui/components/landing/Faq';
-
-const Excalidraw = dynamic(
-  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
-  {
-    ssr: false,
-    loading: () => <Skeleton height="100%" width="100%" />
-  },
-);
-
-function LandingExcalidraw() {
-  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
-  const isSmallScreen = useMediaQuery('(max-width: 1300px)');
-
-  return (
-    <Flex direction="column" justify="center" align="stretch"
-      w={1000} p={20}>
-      <div
-        className={classes.drawing}
-        style={{ width: "100%", height: isSmallScreen ? 300 : 650, borderRadius: "1.0rem", overflow: "hidden", marginTop: "25px" }}>
-        <Excalidraw theme={computedColorScheme}
-          UIOptions={{ tools: { image: false } }}
-          initialData={{
-            elements: WELCOME_EXCALI_DATA.elements as any,
-            scrollToContent: true,
-            appState: { zoom: { value: (isSmallScreen ? .5 : 1) as any } }
-          }} zenModeEnabled />
-      </div>
-    </Flex>
-  );
-}
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Page = () => {
   return <Flex direction="column" w="100%" align="center">
 
-    {/* <Divider my="md" size="xs" w="70%" variant='dashed'
-      color="var(--mantine-color-main-filled)"
-      pos="absolute" top="100px" left="0" /> */}
-
     <Flex wrap="wrap" w="100%" mih={900} justify="center" align="center">
       <LandingHero />
-      <LandingExcalidraw />
+      <Link href="/excalidraw">
+        <Image
+          src="/landing.png"
+          alt="Landing"
+          width={1000}
+          height={650}
+          style={{ borderRadius: "2.0rem", marginTop: "25px", maxWidth: "100%", height: "auto", padding: "20px" }}
+        />
+      </Link>
     </Flex>
 
     <Flex bg="var(--mantine-color-main-light)"
